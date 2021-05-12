0bdc155ebaf442c5f29e1c23bab625d49c203838
Kawtarhssain <82593214+Kawtarhssain@users.noreply.github.com>
2021-05-12 16:33:45 +0200

    Update Calculator.java

--- src/main/java/com/formaton/Calculator.java
+++ src/main/java/com/formaton/Calculator.java
@@ -2,12 +2,12 @@ package com.formaton;
 
 public class Calculator {
 	
-	public int add(int a, int b) {
-		return a+b;
+	public int add(int a, int b, int c) {
+		return a+b+c;
 	}
 	
-	public int multiply(int a, int b) {
-		return a*b;
+	public int multiply(int a, int b, int c) {
+		return a*b*c;
 	}
 
 }
